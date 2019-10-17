c6d0c74b36aa1de287f30aa39f4e09d1d592892f
宮原淳 <jonconan@jonconan-macbook.local>
2019-10-17 19:45:41 +0900

    [add] 認証作りかけ

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,14 +1,27 @@
 class UserSessionsController < ApplicationController
 
   def sign_in
-    render json: "sign_in"
   end
 
   def new_session
-    render json: "new_session"
+    post_data = params[:post]
+
+    @user = login(params[:user_code], params[:password]) if user_check?
+    if @user
+      flash[:alert] = "ログインしました"
+      redirect_to root_path
+    else
+      render json: "NG"
+    end
   end
 
   def sign_out
-    render json: "sign_out"
+  end
+
+  private
+
+  def user_check?
+    user = User.find_by(user_code: post_data["user_code"])
+    user.present?
   end
 end
